3c73105c8de91fa0313c8c2560b41d3a2ac849de
Steve Geluso <stevegeluso@gmail.com>
2017-01-30 11:53:51 -0800

    creating school tool site and README

---
+++ schooltool/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SchooltoolConfig(AppConfig):
+    name = 'schooltool'
---
+++ schooltool/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.db import models
+
+class Course(models.Model):
+  title = models.CharField(max_length=100)
+  max_students = models.IntegerField(default=0)
+  start_date = models.DateField()
+  end_date = models.DateField()
+  
+  def __str__(self):
+    return self.title
+
+class Staff(models.Model):
+  name = models.CharField(max_length=100)
+  course = models.ForeignKey(Course, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.name
+
+class Student(models.Model):
+  name = models.CharField(max_length=100)
+  course = models.ForeignKey(Course, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.name
