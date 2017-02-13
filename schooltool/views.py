9bb5e9a0e0738dada41a3a1502286cb110ac8dd7
brianbixby0@gmail.com <brianbixby0@gmail.com>
2017-02-13 15:23:32 -0800

    profile

--- schooltool/views.py
+++ schooltool/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 
 def index(request):
   return render(request, 'schooltool/index.html')
+
+def profile(request):
+    context = {"error": False}
+    if request.user.is_authenticated:
+        context = {
+            "users": User.objects.filter(user_id = request.user.id).select_related()
+        }
+        return render(request, 'schooltool/profile.html', {})
+    else:
+        context["error"] = "Not authenticated"
+        return render(request, 'schooltool/signup.html', context)
