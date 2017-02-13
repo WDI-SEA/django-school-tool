43b2d06715e98d85d81c3744ebf663844bb39c90
Tobias Murphy <murphy.tobias@gmail.com>
2017-02-13 15:43:17 -0800

    navbar

--- schooltool/views.py
+++ schooltool/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return render(request, 'schooltool/index.html')
+ return render(request, 'schooltool/index.html')
+
+def profile(request):
+   context = {"error": False}
+   if request.user.is_authenticated:
+       return render(request, 'schooltool/secret.html')
+   else:
+       context["error"] = "Not authenticated"
+       return render(request, 'schooltool/signup.html', context)
