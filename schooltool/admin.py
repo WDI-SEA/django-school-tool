2f2d7ce6b749019e03d65095e7cc2226f75dabae
srobertson421 <srobertson421@gmail.com>
2017-02-13 13:13:07 -0800

    Changed DB settings

--- schooltool/admin.py
+++ schooltool/admin.py
@@ -2,6 +2,3 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Course)
-admin.site.register(Staff)
-admin.site.register(Student)
--- schooltoolsite/settings.py
+++ schooltoolsite/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'schooltoolsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'schooltoolsite',
+        'HOST': 'localhost'
     }
 }
 
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
