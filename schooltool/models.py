192e5d351364b99a314d705281338b8362a9eb33
Tommy-Lim <tlim24@gmail.com>
2017-02-13 14:32:28 -0800

    finished building login route and form

--- schooltool/models.py
+++ schooltool/models.py
@@ -1,1 +1,2 @@
 from django.db import models
+from django.contrib.auth.models import User
--- schooltool/views.py
+++ schooltool/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def index(request):
   return render(request, 'schooltool/index.html')
+
+def login(request):
+    context = {"Error": False}
+    if request.method == "GET":
+        return render(request, 'schooltool/login.html')
+    if request.method == "POST":
+        email = request.post["email"]
+        password = request.post["password"]
+        user = auth.authenticated(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            context = {"Error": "User not authenticated"}
+            return redirect('login')
