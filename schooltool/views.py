e74d02e3164087f2049460d16c6d3393faae224e
Tommy-Lim <tlim24@gmail.com>
2017-02-13 15:40:46 -0800

    created upstream branch

--- schooltool/views.py
+++ schooltool/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.contrib import auth
 from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
