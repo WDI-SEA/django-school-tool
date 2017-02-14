bf2566d56a3b9eaa3ce22dac80cf1e54af15aa98
Ryne Schillinger <ryneschillinger@gmail.com>
2017-02-13 16:06:10 -0800

    added create course and edit course pages + routes

--- schooltool/urls.py
+++ schooltool/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    #url(r'^(?P<pk>[0-9]+)/$', views.DetailsView.as_view(), name="details"),
-    #url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name="results"),
-    #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote")
+    # url(r'^(?P<pk>[0-9]+)/$', views.DetailsView.as_view(), name="details"),
+    # url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name="results"),
+    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote"),
+    url(r'^create_course$', views.create_course, name="create_course"),
+    url(r'^courses/(?P<course_id>[0-9]+)/edit$', views.edit_course, name="edit_course")
 ]
--- schooltool/views.py
+++ schooltool/views.py
@@ -1,6 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from .models import Course, Staff, Student
 
 # Create your views here.
 
 def index(request):
-  return render(request, 'schooltool/index.html')
+  context = {
+    "courses": Course.objects.all()
+  }
+  return render(request, 'schooltool/index.html', context)
+
+def create_course(request):
+	if request.method == "GET":
+		context = {
+			"courses": Course.objects.all()
+			# "courses": Course.objects.filter(user_id = request.user.id).select_related()
+		}
+		return render(request, 'schooltool/create_course.html', context)
+	elif request.method == "POST":
+		# if request.user.is_authenticated:
+			new_course = Course()
+			new_course.title = request.POST["title"]
+			new_course.start_date = request.POST["start_date"]
+			new_course.end_date = request.POST["end_date"]
+			new_course.user_id = request.user.id
+			new_course.save()
+			return redirect('index')
+		# else:
+		# 	return redirect('/create_course')
+
+
+def edit_course(request, course_id):
+	if request.user.is_staff:
+		if request.method == "GET":
+			context = {
+				"course": Course.objects.get(pk=course_id)
+			}
+			return render(request, 'schooltool/edit_course.html', context)
+
+		elif request.method == "POST":
+			if request.POST["request"] == "PUT":
+				title = request.POST["title"]
+				max_students = request.POST["max_students"]
+				start_date = request.POST["start_date"]
+				end_date = request.POST["end_date"]
+				Course.objects.filter(pk=course_id).update(title=title, max_students=max_students, start_date=start_date, end_date=end_date)
+				return redirect('/courses/' + course_id + '/edit')
+	else:
+		return redirect('/courses/' + course_id)
