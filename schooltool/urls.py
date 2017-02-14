ec56f5c3564eea45bcc1553df6190d7aab340424
skylocke <mathematical.fiction@gmail.com>
2017-02-13 16:12:21 -0800

    edit course date inputs work

--- schooltool/urls.py
+++ schooltool/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     #url(r'^(?P<pk>[0-9]+)/$', views.DetailsView.as_view(), name="details"),
     #url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name="results"),
     #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote")
+    # url(r'^edit$', views.edit_course, name="edit_course"),
+    url(r'^courses/(?P<course_id>[0-9]+)/edit$', views.edit_course, name="edit_course"),
 ]
--- schooltool/views.py
+++ schooltool/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Course
 # Create your views here.
 
 def index(request):
   return render(request, 'schooltool/index.html')
+
+def edit_course(request, course_id):
+    if request.user.is_staff:
+        if request.method == "GET":
+            context = {
+                "course": Course.objects.get(pk=course_id)
+            }
+            context["course"].start_date = context["course"].start_date.strftime('%Y-%m-%d')
+            context["course"].end_date = context["course"].end_date.strftime('%Y-%m-%d')
+            
+            return render(request, 'schooltool/edit_course.html', context)
+
+        elif request.method == "POST":
+            if request.POST["request"] == "PUT":
+                title = request.POST["title"]
+                max_students = request.POST["max_students"]
+                start_date = request.POST["start_date"]
+                end_date = request.POST["end_date"]
+                Course.objects.filter(pk=course_id).update(title=title, max_students=max_students, start_date=start_date, end_date=end_date)
+                return redirect('/courses/' + course_id + '/edit')
+    else:
+        return redirect('/courses/' + course_id)
