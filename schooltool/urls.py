8c7f02805fea00fc31add8e7b2e349a7a07c2ad6
Donnie Ellstrom <dsvene@gmail.com>
2017-02-13 16:14:41 -0800

    Resolve merge conflicts

--- schooltool/urls.py
+++ schooltool/urls.py
@@ -3,11 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-<<<<<<< HEAD
-    url(r'^courses$', views.courses, name="courses")
-=======
+    url(r'^courses$', views.courses, name="courses"),
     url(r'^profile$', views.profile, name="profile"),
->>>>>>> 3dd47cc3beeb170caf7ebdb473668dd9e29293ea
+
     #url(r'^(?P<pk>[0-9]+)/$', views.DetailsView.as_view(), name="details"),
     #url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name="results"),
     #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote")
--- schooltool/views.py
+++ schooltool/views.py
@@ -4,7 +4,6 @@ from .models import Course
 # Create your views here.
 
 def index(request):
-<<<<<<< HEAD
   return render(request, 'schooltool/index.html')
 
 def courses(request):
@@ -12,14 +11,13 @@ def courses(request):
     "courses" :  Course.objects.all()
   }
   return render(request, 'schooltool/courses.html', context)
-=======
- return render(request, 'schooltool/index.html')
+
+  return render(request, 'schooltool/index.html')
 
 def profile(request):
-   context = {"error": False}
-   if request.user.is_authenticated:
-       return render(request, 'schooltool/secret.html')
-   else:
-       context["error"] = "Not authenticated"
-       return render(request, 'schooltool/signup.html', context)
->>>>>>> 3dd47cc3beeb170caf7ebdb473668dd9e29293ea
+  context = {"error": False}
+  if request.user.is_authenticated:
+    return render(request, 'schooltool/secret.html')
+  else:
+    context["error"] = "Not authenticated"
+    return render(request, 'schooltool/signup.html', context)
